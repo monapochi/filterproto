86f74e6f15cf6a86a3532978aaa3b277ba14857f
直井真一郎 <naoishinichirou@kuriki-no-MacBook-Pro.local>
2014-02-10 16:04:53 +0900

    Initial Commit

---
+++ filterproto/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  filterproto
+//
+//  Created by 直井真一郎 on 2014/02/10.
+//  Copyright (c) 2014年 直井真一郎. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
